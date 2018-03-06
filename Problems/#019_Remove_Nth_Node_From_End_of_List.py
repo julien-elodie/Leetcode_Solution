c4e0e616cefc644ec26569c6bc515f880bed5c59
julien-elodie <2644756656@qq.com>
2018-03-06 09:10:45 +0800

    Day 5 : finish # 017 ~ #020

---
+++ Problems/#019_Remove_Nth_Node_From_End_of_List.py
@@ -0,0 +1,65 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class ListNodeHelper:
+    def __init__(self):
+        pass
+
+    def generate(self, list):
+        """
+        :type list: list
+        :rtype: ListNode
+        """
+        N = node = ListNode(0)
+        for i in list:
+            node.next = ListNode(i)
+            node = node.next
+        return N.next
+
+    def transform(self, node):
+        """
+        :type: ListNode
+        :rtype list: list
+        """
+        ret = []
+        while node:
+            ret.append(node.val)
+            node = node.next
+        return ret
+
+    def print(self, node):
+        """
+        :type node: ListNode
+        """
+        prt = []
+        while node:
+            prt.append(str(node.val))
+            node = node.next
+        print(" -> ".join(prt))
+
+
+class Solution:
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        helper = ListNodeHelper()
+        list = helper.transform(head)
+        del list[-n]
+        return helper.generate(list)
+
+
+if __name__ == "__main__":
+    # Helper
+    helper = ListNodeHelper()
+    head = helper.generate([1, 2, 3, 4, 5])
+
+    s = Solution()
+    output = s.removeNthFromEnd(head, 2)
+    helper.print(output)
---
+++ Problems/#020_Valid_Parentheses.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        bracket_map = {
+            "(": ")",
+            "[": "]",
+            "{": "}"
+        }
+        bracket = ""
+        brackets = []
+        for i in s:
+            if i in "([{":
+                bracket = bracket_map[i]
+                brackets.append(i)
+            elif i == bracket:
+                brackets.pop()
+                if brackets:
+                    bracket = bracket_map[brackets[-1]]
+            else:
+                return False
+        return not bool(brackets)
+
+
+if __name__ == "__main__":
+    s = Solution()
+    # output = s.isValid("[")
+    output = s.isValid("([])")
+    print(output)
